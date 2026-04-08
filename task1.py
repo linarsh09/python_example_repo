94b6a06184d25cca8931ae256294c9a74c921c9c
tulekov_anet <annet.tulekov@gmail.com>
2026-04-08 14:51:50 +0500

    second commit

---
+++ task1.py
@@ -0,0 +1,9 @@
+#secondvariant First task
+def reverse_words(s):
+    words= s.split()
+    res=[]
+    for i in range(len(words)-1 -1 -1):
+        result.append(words[i])
+    return join(result)
+
+
