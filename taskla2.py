372492a4e82b34551fdeb1e6ecfbbdbf646dc478
tulekov_anet <annet.tulekov@gmail.com>
2026-04-15 14:18:01 +0500

    aa

---
+++ taskla2.py
@@ -0,0 +1,22 @@
+class Employee():
+    def __init__(self,name,position,sallary):
+        self.__name= name
+        self.__position=position
+        self.__sallary=sallary
+    def work(self):
+        print("work")
+    def get_bonus(self):
+        if self.__sallary>5000
+    class manager(Employee):
+        def __init__(self):
+            super().__init__()
+        def work(self):
+            print("manager's work")
+
+    class Developer(Employee):
+        def __init__(self):
+            super().__init__()
+        def work(self):
+            print("developer's work")
+        
+
