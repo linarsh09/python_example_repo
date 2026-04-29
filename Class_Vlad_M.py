72872a16a31f5bb38cf20876802d7379dd598f9e
linarsh09 <russkiha885@gmail.com>
2026-04-29 14:32:23 +0500

    test task

---
+++ Class_Vlad_M.py
@@ -0,0 +1,11 @@
+#Var 4
+
+class employee():
+    def __init__(self, name, employee_id, position, salary, work_hours)
+    super.classEmployee():
+    def word_time()
+    self.getname
+    self.getid(employee_id)
+    self.get_total_hours(for work_hours in range)
+
+    class manager(self)
