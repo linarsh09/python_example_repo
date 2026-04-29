bf5615cf048734cb3ea58ee0f3f0b5a2cc209b5a
Malika Amanbekova <k3632076@gmail.com>
2026-04-29 14:44:26 +0500

    mainwindoe for tictactoe

--- mainwindow.py
+++ mainwindow.py
@@ -1,12 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QMessageBox, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QGridLayout, QMessageBox, QVBoxLayout
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.widget = QWidget
+        self.widget = QWidget()
         self.setWindowTitle("Меню")
-        self.setGeometry(200, 200, 200, 200)
+        self.setGeometry(400, 400, 400, 400)
         self.newgame_button = QPushButton("Новая игра")
         self.settings_button = QPushButton("Настройки")
         self.records_button = QPushButton("Рекорд")
@@ -19,4 +19,10 @@ class MainWindow(QWidget):
         self.layout.addWidget(self.settings_button)
         self.layout.addWidget(self.records_button)
         self.layout.addWidget(self.exit_button)
+    
+if __name__ == "__main__":
+    app = QApplication([])
+    main_window = MainWindow()
+    main_window.show()
+    app.exec_()
 
