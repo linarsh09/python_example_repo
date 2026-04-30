0b78e64942a25ad744cf7abf57cc4efc5bc45e72
unknown <vladmashtakov4@gmail.com>
2026-04-30 10:39:46 +0300

    task fixed

--- Classes.py
+++ Classes.py
@@ -1,14 +1,12 @@
 class University: 
-	def __init__(self,name,founded_year,students_count,departments)
+	def __init__(self,name_uni,founded_year,students_count,departments):
+        self.name = name_uni
+        self.founded_year = founded_year
+        self.students_count = students_count
+        self.departments = departments
 
-	self.name = name
-	self.founded_year = founded_year
-	self.students_count = students_count
-	self.departments = departments
-
-#getters
 	def name(self):
-		return self.name
+		return self.name_uni
 	def founded_year(self):
 		return self.founded_year
 	def students_count(self):
@@ -16,33 +14,21 @@ class University:
 	def departments(self):
 		return self.departments
 
-	def add_department(self,dept) #сыро
-		dept = self.departments.append
-		return {dept} 
+    def departad(self,dept):
+	    dept = self.departments.append
+	    return {dept} 
 
-#setters
-
-
-
-
-
-#methods
-	
-	def __str__(self):
-		return
 
 	def get_age(self,founded_year):
-	age = "Абстрактный календарь" - self.founded_year
-	return age
+        age =  calendar - self.founded_year
+	    return get_age
 
 	def get_info(self,name,founded_year,students_count):
-		return (f "Название университета:{self.name}. Основан в {self.founded_year}. Кол.во учащихся:{self.students_count}")
-		
+	    return (f"Название университета:{self.name_uni}. Основан в {self.founded_year}. Кол.во учащихся:{self.students_count}")
 
 	def list_departments(self, departments):
-		for departments in self.departments
-		print print (f "Факультеты: {dept}")
-		return list_departments
+		for departments in self.departments:
+		    return print(f"Факультеты: {dept}")
 	
 	def get_ranking(self):
 		return "Рейтинга нет" 
@@ -51,15 +37,16 @@ class University:
 		return self.department[index]
 
 class TechicalUniversity(University):
-	def __init__(self,name,founded_year,students_count,departments,specializations,ranking,teaching_age)
-		super().__init__(name,founded_year,students_count,departments,specializations,ranking)
-	self.ranking = ranking
-	self.name = name
-	self.founded_year = founded_year
-	self.students_count = students_count
-	self.departments = departments
-	self.specializations = specializations
-	self.teaching_age = teaching_age
+	def __init__(self,name_uni,founded_year,students_count,departments,specializations,ranking_uni,teaching_age):
+	    super().__init__(name_uni,founded_year,students_count,departments,specializations,)
+
+        self.ranking = ranking_uni
+        self.name = name_uni
+        self.founded_year = founded_year
+        self.students_count = students_count
+        self.departments = departments
+        self.specializations = specializations
+        self.teaching_age = teaching_age
 
 #getters
 	def name(self):
@@ -70,8 +57,7 @@ class TechicalUniversity(University):
 		return self.students_count
 	def departments(self):
 		return self.departments
-
-	def specs(self,specializations)
+	def specs(self):
 		retrun self.specializations
 
 	
@@ -80,8 +66,8 @@ class TechicalUniversity(University):
 	
 
 	def get_age(self,founded_year):
-	age = "Абстрактный календарь" - self.founded_year
-	return age
+        age = "Абстрактный календарь" - self.founded_year
+        return age
 
 	def get_info(self,name,founded_year,students_count):
 		return (f "Название университета:{self.name}. Основан в {self.founded_year}. Кол.во учащихся:{self.students_count}")
@@ -89,17 +75,18 @@ class TechicalUniversity(University):
 
 	def list_departments(self, departments):
 		for departments in self.departments
-		print print (f "Факультеты: {dept}")
+		print (f "Факультеты: {dept}")
 		return list_departments
 	
 	def get_ranking(self):
-		return (f Рейтинг университета {self.ranking}) 
+		return(f "Рейтинг университета {self.ranking}") 
 	
 	def __getitem__(self,index):
 		return self.department[index]
 
 	def __contains__(self,dept):
-		for dept in self.
+		for dept in self.department
+
 	
 	
 		
