c8b9676199591bf92aa1bc0348b8219a680086c4
Тимур Османов <ospanov.timur2006@gmail.com>
2026-05-06 14:18:02 +0500

    1 laba

---
+++ 2.py
@@ -0,0 +1,12 @@
+# первая лаба
+
+def sum(k, n):
+    def mult(j, k):
+        if j > k:
+            return 1
+        return ((e ** (1 / j)) / j) * mult(j + 1, k)
+    if k > n:
+        return 0
+    return mult(1, k) + sum(k + 1, n)
+n = int(input("Введите n: "))
+print(sum(1, n))
